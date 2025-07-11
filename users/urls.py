cef772357f8027ee03f62762ee9bdb1ce3e7e038
João Henrique <joaohr99@gmail.com>
2025-07-11 20:06:47 -0300

    Detalhes

--- users/urls.py
+++ users/urls.py
@@ -46,3 +46,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+## Linha para comentários.
+
